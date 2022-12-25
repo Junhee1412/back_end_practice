0ef6d97072676a783bd2b43455fea5b4d6560cf1
준희 <118590361+Junhee1412@users.noreply.github.com>
2022-12-25 21:23:08 +0900

    1225 study commit

--- study/01.spring/hello-spring/src/main/java/ezen/hellospring/controller/HelloController.java
+++ study/01.spring/hello-spring/src/main/java/ezen/hellospring/controller/HelloController.java
@@ -1,8 +1,11 @@
 package ezen.hellospring.controller;
 
+import ezen.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +14,36 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //"hello spring"
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
---
+++ study/01.spring/hello-spring/src/test/java/ezen/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package ezen.hellospring.repository;
+
+import ezen.hellospring.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        Assertions.assertEquals(member, result);
+    }
+}
