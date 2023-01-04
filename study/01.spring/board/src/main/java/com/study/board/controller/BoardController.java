d43073c82240906d2477cb4d1bce6e204beda784
준희 <118590361+Junhee1412@users.noreply.github.com>
2023-01-05 00:49:01 +0900

    230105 study commit

---
+++ study/01.spring/board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,48 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")//localhost:8080/board/write
+    public String boardWriteForm(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writedo")
+    public String boardWritePro(Board board){
+        boardService.write(board);
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardview(Model model, Integer id){
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+}
--- study/01.spring/core/src/main/java/hello/core/MemberApp.java
+++ study/01.spring/core/src/main/java/hello/core/MemberApp.java
@@ -1,9 +1,6 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.*;
 
 public class MemberApp {
 
