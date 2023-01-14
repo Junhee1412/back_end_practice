7f835d42110280c55f49b62cec0f0e6532c79cbe
준희 <118590361+Junhee1412@users.noreply.github.com>
2023-01-14 21:32:22 +0900

    team project front commit

---
+++ study/01.spring/Meow/src/test/java/com/ezen/Meow/MeowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.Meow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
