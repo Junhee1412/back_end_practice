8af551b6777c06f1e739bd06228706ec514d3b6f
준희 <118590361+Junhee1412@users.noreply.github.com>
2023-08-02 14:40:27 +0900

    0802 : study, 애플리케이션 테스트의 분류 (2과목) vue Project Name : blog, 라우터 문법 시작

---
+++ study/06.vuejs/blog/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  안녕
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
